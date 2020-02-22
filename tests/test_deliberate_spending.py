5b17c7b9bb68b2acaa5f4c0bbd873d8908dc37c1
Gen Ohta <genohta@Gens-MBP.fios-router.home>
2020-02-22 00:33:54 -0500

    ensure response code is 200

--- tests/test_deliberate_spending.py
+++ tests/test_deliberate_spending.py
@@ -8,5 +8,6 @@ def client():
         yield client
 
 def test_index(client):
-    rv = client.get('/')
-    assert b"Click the button below to open a list of Institutions. After you select one, you'll be guided through an authentication process." in rv.data
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Click the button below to open a list of Institutions. After you select one, you'll be guided through an authentication process." in response.data
