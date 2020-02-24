09bc08f14c6555e00d48d097397cec52819ff1ac
Gen Ohta <genohta@Gens-MBP.fios-router.home>
2020-02-23 21:53:31 -0500

    test auth and transaction endpoints

--- tests/test_deliberate_spending.py
+++ tests/test_deliberate_spending.py
@@ -3,6 +3,7 @@ import os
 import pytest
 
 from deliberate_spending import app
+from deliberate_spending.views import access_token
 
 @pytest.fixture
 def client():
@@ -14,9 +15,24 @@ def test_index(client):
     response = client.get('/')
     assert response.status_code == 200
 
-def test_get_access_token(client, monkeypatch):
-    public_token = os.getenv('PUBLIC_TOKEN')
+# TODO test unhappy path for each API endpoint
+
+# TODO test helper methods
+
+def test_get_access_token(client):
+    public_token = os.getenv('TEST_PUBLIC_TOKEN')
     data = { "public_token": public_token }
     response = client.post('/get_access_token', data=data)
     assert response.status_code == 200
+
+def test_auth(client):
+    access_token = os.getenv('TEST_ACCESS_TOKEN')
+    response = client.get('/auth')
+    assert response.status_code == 200
+
+def test_transactions(client):
+    access_token = os.getenv('TEST_ACCESS_TOKEN')
+    response = client.get('/transactions')
+    assert response.status_code == 200
+
 
